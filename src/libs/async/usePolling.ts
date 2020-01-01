a4fe557eba81b9cff4f85a781df0a1133778128f
chenzhaolong <1332588507@qq.com>
2020-01-02 00:17:30 +0800

    初步完成usePolling钩子

--- src/libs/async/usePolling.ts
+++ src/libs/async/usePolling.ts
@@ -1,9 +1,12 @@
 /**
  * @file the polling fetch of react hook
+ * fit to the params of callback is not changed;
+ * todo: 可以提供选项限制usePolling的轮询次数
  */
 import { useState, useCallback } from 'react';
-import { isFunction } from 'lodash';
+import { isFunction, isBoolean } from 'lodash';
 import { error } from '../../utils/log';
+import { isPromise } from '../../utils/tools';
 
 interface Polling {
   result: any;
@@ -13,25 +16,49 @@ interface Polling {
 interface Options {
   timeout?: number;
   terminate: (data: any) => boolean;
-  deps?: Array<any>;
   initValue?: any;
 }
 
 type Func = () => any;
 
 function usePolling(callback: Func, options: Options): Polling {
-  const { timeout = 0, ternimate, deps = [], initValue = {} } = options;
-  if (!isFunction(ternimate)) {
+  const { timeout = 0, terminate, initValue = {} } = options;
+  const defaultResult = { result: {}, start: () => {} };
+  if (!isFunction(terminate)) {
     error('the terminate of options must be exist.');
-    return { result: {}, start: () => {} };
+    return defaultResult;
   }
+  if (!isFunction(callback)) {
+    error('the first params of input must be function type.');
+    return defaultResult;
+  }
+
   const [result, setResult] = useState(initValue || {});
   const start = useCallback(() => {
-    // @ts-ignore
-    callback && callback.call(null, callback.arguments, changeData);
+    const promise = callback();
+    if (!isPromise(promise)) {
+      error('the first params of input must be Promise.');
+    } else {
+      promise
+        .then((response: any) => {
+          const isNotPolling = terminate(response);
+          if (!isBoolean(isNotPolling)) {
+            error('the return of terminate must be Boolean type.');
+          } else {
+            if (isNotPolling) {
+              setResult(response);
+            } else {
+              setTimeout(start, timeout);
+            }
+          }
+        })
+        .catch((e: any) => {
+          throw e;
+        });
+    }
   }, []);
 
-  return { result, start: start.bind(null, setResult) };
+  return { result, start };
 }
 
 export default usePolling;
