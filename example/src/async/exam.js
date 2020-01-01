7132f5fbd96fcebf4ced54f98238f77104e6d9bc
chenzhaolong <1332588507@qq.com>
2020-01-02 01:11:40 +0800

    补充usePolling的例子

--- example/src/async/exam.js
+++ example/src/async/exam.js
@@ -2,7 +2,7 @@
  * @file check the async of hooks
  */
 import React, {Component, useEffect, useState} from 'react'
-import {useFetchForMount, useFetchAll} from '../../react-sweet/src';
+import {useFetchForMount, useFetchAll, usePolling} from '../../react-sweet/src';
 
 export class Async extends Component {
   state = {
@@ -13,16 +13,21 @@ export class Async extends Component {
     return (
       <div>
         {/*<CheckMountFetch />*/}
-        <CheckFetchAll id={this.state.changeId}/>
+        {/*<CheckFetchAll id={this.state.changeId}/>*/}
+        <CheckPolling/>
       </div>
     )
   }
 }
 
+let times = 0
+
 function fetch (params) {
   return new Promise((res, rej) => {
     setTimeout(() => {
-      return res({a: {b: {c: params}},d: params})
+      console.log('一次');
+      times = times + 1
+      return res({a: {b: {c: params}},d: times})
     }, 1000);
   })
 }
@@ -53,5 +58,23 @@ function CheckFetchAll(props) {
     <p>apple: {data.apple.d}</p>
     <p>banana: {data.banana.d}</p>
     <p>orange: {data.orange.a.b.c}</p>
+  </div>
+}
+
+function CheckPolling (props) {
+  const {result, start} = usePolling(() => {
+    return fetch('1');
+  }, {
+    terminate(response) {return response.d > 3},
+    timeout: 1000,
+    initValue: {a: {b: {c: ''}}}
+  });
+
+  useEffect(() => {
+    start()
+  }, []);
+
+  return <div>
+    <p>结果: {result.a.b.c}</p>
   </div>
 }
