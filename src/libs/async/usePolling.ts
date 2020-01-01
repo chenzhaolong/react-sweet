401b23859bdd0e129ee24b8e8f4f81b950e7c1ad
chenzhaolong <1332588507@qq.com>
2020-01-01 18:06:45 +0800

    初步指定usePolling的参数格式

--- src/libs/async/usePolling.ts
+++ src/libs/async/usePolling.ts
@@ -2,23 +2,31 @@
  * @file the polling fetch of react hook
  */
 import { useState, useCallback } from 'react';
-// import {isNumber} from 'lodash';
-// import { error } from '../../utils/log';
+import { isFunction } from 'lodash';
+import { error } from '../../utils/log';
 
 interface Polling {
   result: any;
   start: () => any;
 }
 
+interface Options {
+  timeout?: number;
+  terminate: (data: any) => boolean;
+  deps?: Array<any>;
+  initValue?: any;
+}
+
 type Func = () => any;
 
-function usePolling(callback: Func, initValue?: any): Polling {
-  // if (!isNumber(time)) {
-  //   error('the second input must be exist and must be number');
-  //   return {result: '', start: () => {}};
-  // }
+function usePolling(callback: Func, options: Options): Polling {
+  const { timeout = 0, ternimate, deps = [], initValue = {} } = options;
+  if (!isFunction(ternimate)) {
+    error('the terminate of options must be exist.');
+    return { result: {}, start: () => {} };
+  }
   const [result, setResult] = useState(initValue || {});
-  const start = useCallback((changeData) => {
+  const start = useCallback(() => {
     // @ts-ignore
     callback && callback.call(null, callback.arguments, changeData);
   }, []);
