88a7ab265896785989d3a287bfef1abde69eb6bd
chenzhaolong <1332588507@qq.com>
2020-01-18 11:12:51 +0800

    为useLifeStatus钩子添加追踪deps

--- src/libs/lifeCycle/useLifeStatus.ts
+++ src/libs/lifeCycle/useLifeStatus.ts
@@ -1,17 +1,24 @@
 /**
  * @file get the status of life cycle for functional component
+ * todo:后续可以根据某些依赖来计算出他的update次数
  */
 
-import { useEffect, useRef } from 'react';
+import { useMemo, useRef, useCallback } from 'react';
+import { isObject } from 'lodash';
+import { error } from '../../utils/log';
 
 interface Value {
   status: string;
   updateTimes: number;
+  deps?: Array<string>;
 }
 
-function useLifeStatus(deps?: Array<any>): Value {
-  const render = useRef({ status: 'mount', updateTimes: 0, deps });
-  useEffect(() => {
+type a = { [index: string]: any };
+
+function useLifeStatus(trace?: a): Value {
+  const render = useRef({ status: 'mount', updateTimes: 0, deps: [] });
+
+  const fn = useCallback(() => {
     if (render.current.status === 'mount') {
       render.current.status = 'mounted';
     } else if (render.current.status === 'mounted') {
@@ -20,8 +27,25 @@ function useLifeStatus(deps?: Array<any>): Value {
     } else if (render.current.status === 'update') {
       render.current.updateTimes = render.current.updateTimes + 1;
     }
-  }, deps);
-  return render.current;
+    return render.current;
+  }, []);
+
+  if (trace) {
+    if (!isObject(trace)) {
+      error('trace of input param must be Object type!');
+      return { status: '', updateTimes: 0 };
+    }
+    const depsKey: Array<string> = Object.keys(trace);
+    // @ts-ignore
+    render.current.deps = depsKey;
+    // @ts-ignore
+    const depsValue = depsKey.map((key: string) => trace[key]);
+    return useMemo(() => {
+      return fn();
+    }, depsValue);
+  } else {
+    return fn();
+  }
 }
 
 export default useLifeStatus;
