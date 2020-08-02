363ea6526a08242782835f78d1ca48f62464ad3d
chenzhaolong <1332588507@qq.com>
2020-08-03 01:20:32 +0800

    重写了useFetchAll的逻辑和结构

--- src/libs/async/useFetchAll.ts
+++ src/libs/async/useFetchAll.ts
@@ -3,53 +3,142 @@
  * todo: 后续为每一个key添加一个path，获取指定的数据
  * todo: 后续将每个配置项集中起来，并加上onError
  */
-import { useState, useEffect } from 'react';
-import { isObject } from 'lodash';
+import { useState, useEffect, useMemo, useCallback, useRef } from 'react';
+import { isObject, isFunction } from 'lodash';
 import { error } from '../../utils/log';
 import { isPromise } from '../../utils/tools';
 
-type FetchList = { [key: string]: () => any };
+type FetchList = { [key: string]: () => Promise<any> };
 type Obj = { [key: string]: any };
 
-function useFetchAll(fetchList: FetchList, initValue?: Obj, deps: Array<any> = []): object {
+enum AutoFetchTimeType {
+  MOUNT = 'mount',
+  UPDATE = 'update'
+}
+
+interface Options {
+  initValue?: { [key: string]: any };
+  onError?: (e: Error) => any;
+  onSuccess?: (data: object, setData: (data: any) => void) => void;
+  closeLoading?: boolean;
+  deps?: Array<any>;
+  // autoFetch?: boolean;
+  autoFetchMoment?: AutoFetchTimeType;
+}
+
+interface Result {
+  readonly response?: object;
+  // startFetch?: () => void;
+  loading?: boolean;
+}
+
+function useFetchAll(fetchList: FetchList, options: Options): Result {
   if (!isObject(fetchList)) {
     error('make sure the input is object');
-    return {};
   }
-  const fetchKey = Object.keys(fetchList);
-  // todo: 可能存在执行多次fetchList里面每个key函数
-  const result = fetchKey.every((key) => {
-    return isPromise(fetchList[key]);
-  });
-  if (!result) {
-    error('make sure each value of key of the first input is Promise');
-    return {};
-  }
-  let realInitValue: Obj = {};
-  if (initValue) {
-    realInitValue = initValue;
-  } else {
-    fetchKey.forEach((key) => {
-      realInitValue[key] = {};
+  const fetchKey = useMemo(() => {
+    return Object.keys(fetchList);
+  }, []);
+  const isKeyValuePromise = useMemo(() => {
+    return fetchKey.every((key) => {
+      return isPromise(fetchList[key]);
     });
+  }, []);
+  if (!isKeyValuePromise) {
+    error('make sure each value of key of the first input is Promise');
   }
-  const [data, renderData] = useState(realInitValue);
+
+  const { initValue, onError, onSuccess, autoFetchMoment, closeLoading = false, deps = [] } = options;
+  const [loading, setLoading] = useState(false);
+  const [response, setResponse] = useState(initValue || {});
+  const render = useRef({ isFirstRender: true });
+  const loadingFn = useCallback((isLoading) => {
+    !closeLoading && setLoading(isLoading);
+  }, []);
+
+  const realDeps = autoFetchMoment === AutoFetchTimeType.MOUNT ? [] : deps;
+
   useEffect(() => {
+    if (autoFetchMoment && autoFetchMoment === AutoFetchTimeType.UPDATE && render.current.isFirstRender) {
+      render.current.isFirstRender = false;
+      return;
+    }
+
     const promiseList = fetchKey.map((key) => fetchList[key]);
+    loadingFn(true);
     Promise.all(promiseList)
-      .then((response: Array<any>) => {
+      .then((data: Array<any>) => {
+        loadingFn(false);
         const result: Obj = {};
-        response.forEach((data, index) => {
+        data.forEach((item, index) => {
           const key = fetchKey[index];
-          result[key] = data;
+          result[key] = item;
         });
-        renderData(result);
+        if (isFunction(onSuccess)) {
+          onSuccess(result, setResponse);
+        } else {
+          setResponse(result);
+        }
       })
       .catch((e) => {
-        throw e;
+        loadingFn(false);
+        if (isFunction(onError)) {
+          const data = onError(e);
+          setResponse(data || response);
+        } else {
+          throw e;
+        }
       });
-  }, deps);
-  return data;
+  }, realDeps);
+
+  return { response, loading };
 }
+
+// function useFetchAll(fetchList: FetchList, initValue?: Obj, deps: Array<any> = []): object {
+//   if (!isObject(fetchList)) {
+//     error('make sure the input is object');
+//   }
+//   if (initValue && isArray(initValue) && !deps) {
+//     deps = initValue;
+//     initValue = {};
+//   }
+//   if (!isArray(deps)) {
+//     error('deps must be array');
+//   }
+//   const fetchKey = Object.keys(fetchList);
+//   // todo: 可能存在执行多次fetchList里面每个key函数
+//   const isKeyValuePromise = fetchKey.every((key) => {
+//     return isPromise(fetchList[key]);
+//   });
+//   if (!isKeyValuePromise) {
+//     error('make sure each value of key of the first input is Promise');
+//   }
+//   let realInitValue: Obj = useMemo(() => {
+//     let target = {};
+//     fetchKey.forEach((key: string) => {
+//       // @ts-ignore
+//       target[key] = initValue[key] ? initValue[key] : {};
+//     });
+//     return target
+//   }, []);
+//
+//   const [data, renderData] = useState(realInitValue);
+//   useEffect(() => {
+//     const promiseList = fetchKey.map((key) => fetchList[key]);
+//     Promise.all(promiseList)
+//       .then((response: Array<any>) => {
+//         const result: Obj = {};
+//         response.forEach((data, index) => {
+//           const key = fetchKey[index];
+//           result[key] = data;
+//         });
+//         renderData(result);
+//       })
+//       .catch((e) => {
+//         throw e;
+//       });
+//   }, deps);
+//   return data;
+// }
 
 export default useFetchAll;
