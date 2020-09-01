644f3147c8f6396c7aa9a8e43c47dcf89631394d
chenzhaolong <1332588507@qq.com>
2020-09-02 02:04:17 +0800

    修该变量名称

--- src/plugins/logPlugins.ts
+++ src/plugins/logPlugins.ts
@@ -6,10 +6,10 @@ type Action = { type: string; payload: any };
 type Dispatch = (action: Action) => any;
 
 export function logPlugins(globalState: object) {
-  return (nextDispatch: Dispatch) => {
+  return (rootDispatch: Dispatch) => {
     return (action: Action) => {
       console.log(`${action.type}-prevState:`, globalState);
-      nextDispatch(action);
+      rootDispatch(action);
       console.log(`${action.type}-nextState:`, globalState);
     };
   };
