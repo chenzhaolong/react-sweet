878508c1d54c9ca2b3ba87059aae713ba5891fa3
chenzhaolong <1332588507@qq.com>
2020-09-06 18:02:12 +0800

    修改logPlugins的输出内容

--- src/plugins/logPlugins.ts
+++ src/plugins/logPlugins.ts
@@ -1,6 +1,8 @@
 /**
  * @file 日志插件
  */
+const Colors = require('colors');
+
 type Action = { type: string; payload: any };
 
 type Dispatch = (action: Action) => any;
@@ -8,9 +10,9 @@ type Dispatch = (action: Action) => any;
 export function logPlugins(globalState: object) {
   return (rootDispatch: Dispatch) => {
     return (action: Action) => {
-      console.log(`${action.type}-prevState:`, globalState);
+      console.log(Colors.green(`${action.type}-prevState: ${globalState}`));
       rootDispatch(action);
-      console.log(`${action.type}-nextState:`, globalState);
+      console.log(Colors.green(`${action.type}-nextState: ${globalState}`));
     };
   };
 }
