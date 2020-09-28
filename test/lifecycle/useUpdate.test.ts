a338ffe4096ff0a9d36d0bf213be9bd641187ee2
chenzhaolong <1332588507@qq.com>
2020-09-29 01:30:28 +0800

    完成useUpdate的单测

--- test/lifecycle/useUpdate.test.ts
+++ test/lifecycle/useUpdate.test.ts
@@ -24,4 +24,34 @@ describe('test useUpdate', () => {
     unmount();
     expect(fn).toHaveBeenCalledTimes(2);
   });
+
+  it('useUpdate will throw error when the deps is empty array ', () => {
+    const fn = jest.fn();
+    const { rerender } = renderHook(() => {
+      return useUpdate(fn, []);
+    });
+    rerender();
+    expect(fn).toHaveBeenCalledTimes(0);
+  });
+
+  it('the function can only invoke when the deps changed', () => {
+    const fn = jest.fn();
+    const { rerender } = renderHook(
+      (params) => {
+        return useUpdate(fn, [params.a]);
+      },
+      { initialProps: { a: 1 } }
+    );
+
+    expect(fn).toHaveBeenCalledTimes(0);
+
+    rerender({ a: 2 });
+    expect(fn).toHaveBeenCalledTimes(1);
+
+    rerender({ a: 2 });
+    expect(fn).toHaveBeenCalledTimes(1);
+
+    rerender({ a: 3 });
+    expect(fn).toHaveBeenCalledTimes(2);
+  });
 });
